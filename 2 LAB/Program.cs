170ce55e0f3ffdecf28057c60fec90c83e5bf51d
angela chaj <angelachaj648@gmail.com>
2026-04-27 18:52:18 -0600

    validacion de datos con if ,else

--- 2 LAB/Program.cs
+++ 2 LAB/Program.cs
@@ -10,3 +10,29 @@ Console.Write("Ingrese la cantidad de días: ");
 int dias = int.Parse(Console.ReadLine());
 
 int[,] ventas = new int[productos, dias];
+for (int i = 0; i < productos; i++)
+{
+	for (int j = 0; j < dias; j++)
+	{
+		Console.Write($"Ingrese ventas del producto {i + 1}, día {j + 1}: ");
+		ventas[i, j] = int.Parse(Console.ReadLine());
+	}
+}
+Console.Write("Ingrese el número de producto que desea consultar: ");
+
+int productoSeleccionado = int.Parse(Console.ReadLine());
+if (productoSeleccionado < 1 || productoSeleccionado > productos)
+{
+	Console.WriteLine("Producto inválido.");
+}
+else
+{
+	Console.WriteLine($"Ventas del producto {productoSeleccionado}:");
+
+	int fila = productoSeleccionado - 1;
+
+	for (int j = 0; j < dias; j++)
+	{
+		Console.WriteLine($"Día {j + 1}: {ventas[fila, j]}");
+	}
+}
