e5bf7fd29c80a8017b0e168b599223f7cb7a3130
angela chaj <angelachaj648@gmail.com>
2026-04-27 21:19:04 -0600

    el valor utilizando if y 2 ciclos for

--- 5/Program.cs
+++ 5/Program.cs
@@ -3,17 +3,35 @@
 
 //Solicite al usuario las dimensiones del inventario (filas y columnas).
 // Luego de ingresar los valores (códigos o cantidades), pida un número a buscar. El sistema debe indicar si el valor existe y en qué posición se encuentra (fila y columna).
-Console.Write("Ingrese el número de estudiantes: ");
-int estudiantes = int.Parse(Console.ReadLine());
 
-Console.Write("Ingrese el número de evaluaciones: ");
-int evaluaciones = int.Parse(Console.ReadLine());
+Console.Write("Ingrese el número de filas (estantes): ");
+int filas = int.Parse(Console.ReadLine());
 
-double[,] calificaciones = new double[estudiantes, evaluaciones];
-for(int i = 0; i < estudiantes; i++)
-        {
-	for (int j = 0; j < evaluaciones; j++)
+Console.Write("Ingrese el número de columnas (niveles): ");
+int columnas = int.Parse(Console.ReadLine());
+
+int[,] inventario = new int[filas, columnas];
+
+
+for (int i = 0; i < filas; i++)
+{
+	for (int j = 0; j < columnas; j++)
 	{
-		Console.Write($"Ingrese la calificación del estudiante {i + 1}, evaluación {j + 1}: ");
-		calificaciones[i, j] = double.Parse(Console.ReadLine());
+		Console.Write($"Ingrese el valor en la posición [{i + 1},{j + 1}]: ");
+		inventario[i, j] = int.Parse(Console.ReadLine());
 	}
+}
+Console.Write("Ingrese el número a buscar: ");
+int buscado = int.Parse(Console.ReadLine());
+
+bool encontrado = false;
+
+for (int i = 0; i < filas; i++)
+{
+	for (int j = 0; j < columnas; j++)
+	{
+		if (inventario[i, j] == buscado)
+		{ 		
+
+			
+
