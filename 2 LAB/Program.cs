a6b0214c051595465deb7b2d5b77b0756ea4b979
angela chaj <angelachaj648@gmail.com>
2026-04-27 18:47:29 -0600

    FOR para ingreso de datos

---
+++ 2 LAB/Program.cs
@@ -0,0 +1,12 @@
+﻿//Ejercicio 2 – Ventas de productos por día
+//Un negocio desea analizar las ventas de sus productos durante la semana.
+
+//Solicite al usuario el número de productos y el número de días
+//. Luego de llenar la matriz con las cantidades vendidas, pida al usuario que indique un producto específico (fila) y muestre únicamente las ventas correspondientes a ese producto.
+Console.Write("Ingrese la cantidad de productos: ");
+int productos = int.Parse(Console.ReadLine());
+
+Console.Write("Ingrese la cantidad de días: ");
+int dias = int.Parse(Console.ReadLine());
+
+int[,] ventas = new int[productos, dias];
